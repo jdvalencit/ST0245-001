b8f60783dc518115c2e19bbdd1c2c8843fff4504
Juan David Valencia <61478711+jdvalencit@users.noreply.github.com>
2020-07-31 22:46:20 -0500

    Add files via upload

---
+++ talleres/taller01/Contador.java
@@ -0,0 +1,28 @@
+public class Contador {
+
+    private int cuenta;
+    private final String id;
+
+    public Contador(String id) {
+        this.id = id;
+        cuenta = 0;
+    }
+
+    public void incrementar() {
+        cuenta += 1;
+    }
+
+    public void decrementar() {
+
+        cuenta -= 1;
+    }
+
+    public int incrementos() {
+        return cuenta;
+    }
+
+    public String toString() {
+
+        return this.id + "-" + this.cuenta;
+    }
+}
---
+++ talleres/taller01/Fecha.java
@@ -0,0 +1,53 @@
+public class Fecha {
+
+    private final int dia;
+    private final int mes;
+    private final int anyo;
+
+    public Fecha(int dia, int mes, int anyo){
+
+        this.anyo = anyo;
+        this.mes = mes;
+        this.dia = dia;
+    }
+
+    public int getDia(){
+
+        return this.dia;
+    }
+
+    public int getMes(){
+
+        return this.mes;
+    }
+
+    public int getAnyo(){
+
+        return this.anyo;
+    }
+
+    public String toString(){
+
+        return Integer.toString(this.getAnyo()) + String.format("%02d", this.getMes()) + String.format("%02d", this.getDia());
+    }
+
+    public int comparar(Fecha otraFecha){
+        Long fecha1 = Long.parseLong(this.toString());
+        long fecha2 = Long.parseLong(otraFecha.toString());
+
+        if(fecha1 > fecha2){
+
+            return 1;
+
+        }else if(fecha1 < fecha2){
+
+            return -1;
+
+        }else {
+
+            return 0;
+
+        }
+
+    }
+}
